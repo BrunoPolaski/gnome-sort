b9bf39dfebc6f306615a3e6971d2fdd696863dc0
wOL-Lucas <lucasefernandes333@gmail.com>
2024-03-26 09:30:18 -0300

    Implement sort endpoint on app
    Fix requirements.txt
    Create yield_gnome_sort function.

--- algorithms.py
+++ algorithms.py
@@ -1,15 +1,32 @@
-
-def gnome_sort(arr:list) -> list:
+def gnome_sort(arr: list) -> list:
     i = 0
     while i < len(arr):
         if i == 0:
             i += 1
-        elif(arr[i] >= arr[i - 1]):
+        elif arr[i] >= arr[i - 1]:
             i += 1
-        elif(arr[i] < arr[i -1]):
-            arr[i], arr[i -1] = arr[i -1], arr[i]
+        elif arr[i] < arr[i - 1]:
+            arr[i], arr[i - 1] = arr[i - 1], arr[i]
             i -= 1
 
     return arr
 
 
+def yield_gnome_sort(arr: list):
+
+    indexes = {}
+    i = 0
+    while i < len(arr):
+        if i == 0:
+            i += 1
+        elif arr[i] >= arr[i - 1]:
+            i += 1
+        elif arr[i] < arr[i - 1]:
+            arr[i], arr[i - 1] = arr[i - 1], arr[i]
+            i -= 1
+
+        for index, item in enumerate(arr):
+            indexes[str(index)] = item
+
+        yield indexes
+        indexes = {}
--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 from fastapi import FastAPI, staticfiles
+from pydantic import BaseModel
+from algorithms import yield_gnome_sort
+
+
+class SortRequest(BaseModel):
+    array: list
+
 
 app = FastAPI()
 
 app.mount("/views", staticfiles.StaticFiles(directory="views"), name="views")
+
+
+@app.post("/sort")
+async def sort(data: SortRequest):
+    indexes = [data for data in yield_gnome_sort(data.array)]
+    return indexes

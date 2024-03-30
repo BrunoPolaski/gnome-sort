f963eda26804de336a67f822800baf6815d8ae79
Bruno Polaski <polaskibruno03@gmail.com>
2024-03-30 15:39:12 -0300

    Layout, animations

--- script.ts
+++
@@ -1,9 +0,0 @@
-const webSocket = new WebSocket('ws://localhost:8080');
-
-webSocket.onopen = () => {
-    console.log('Connection established');
-};
-
-webSocket.onmessage = (message) => {
-    console.log('Message received:', message.data);
-}
--- views/src/script.js
+++ views/src/script.js
@@ -66,7 +66,7 @@ const ordenate_array_elements = (steps) => {
         });
       }
     }, delay);
-    delay += 1000;
+    delay += 1000
   });
 };
 

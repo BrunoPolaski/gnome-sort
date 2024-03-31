bf47badf12418960a4a51fc29fae21957d57662e
wOL-Lucas <lucasefernandes333@gmail.com>
2024-03-30 23:35:34 -0300

    Implement webview

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from tests import test
-import algorithms as alg
+import webview
+from dotenv import load_dotenv
+import os
 
-test.test(alg.gnome_sort)
+if __name__ == '__main__':
+    load_dotenv()
+    
+    url = os.getenv('REMOTE_HOST')
+    url = url if url else 'http://localhost:8000/views/index.html'
+    
+    webview.create_window('Gnome Sort', url, width=1920, height=1080)
+    webview.start()
+
